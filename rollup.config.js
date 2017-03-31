9ca8a50e90d6d3d07a558be2fab881549a6bc93c
Filipe Silva <filipematossilva@gmail.com>
2017-03-31 17:23:28 +0100

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import rollup from 'rollup';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  entry: 'src/main.js',
+  dest: 'dist/build.js',
+  sourceMap: false,
+  format: 'iife',
+  plugins: [
+    nodeResolve({ jsnext: true, module: true }),
+    commonjs({
+      include: 'node_modules/rxjs/**',
+    })
+  ]
+}
